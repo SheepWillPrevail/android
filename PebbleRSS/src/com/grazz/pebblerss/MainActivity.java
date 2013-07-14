f7ba489397de82edb0860e949d9b84240806a604
Robert Meijer <robert@grazz.com>
2013-07-14 13:33:01 +0200

    Giving up on AppMessages for a while...

--- PebbleRSS/src/com/grazz/pebblerss/MainActivity.java
+++ PebbleRSS/src/com/grazz/pebblerss/MainActivity.java
@@ -1,7 +1,9 @@
 package com.grazz.pebblerss;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import java.util.UUID;
 
 import android.app.Activity;
@@ -22,28 +24,42 @@ public class MainActivity extends Activity {
 	private PebbleDataReceiver _receiver = new PebbleDataReceiver(APP_UUID) {
 
 		private static final int maxLength = 96;
-		private int _lastTransactionId = 0;
+		private Queue<PebbleDictionary> _msgQueue = new ArrayDeque<PebbleDictionary>();
+		private int _transactionId = 0;
 		private Long _lastFeed;
+
+		private String substring(String str, int maxLength) {
+			int length = str.length();
+			if (length > maxLength)
+				length = maxLength;
+			return str.substring(0, length);
+		}
 
 		@Override
 		public void receiveData(Context context, int transactionId, PebbleDictionary data) {
 			Log.d("receiveData", data.toJsonString());
-			Log.d("ff", Integer.toString(transactionId));
-
 			PebbleKit.sendAckToPebble(context, transactionId);
 
 			Long command_id = data.getUnsignedInteger(1090);
-			if (command_id != null) { // hello
-				Integer total = _feeds.size();
-				if (total > 16)
-					total = 16; // clamp
-				for (int i = 0; i < total; i++) {
-					PebbleDictionary feed_dict = new PebbleDictionary();
-					feed_dict.addString(1001, substring(_feeds.get(i).getName(), maxLength));
-					feed_dict.addUint8(1011, total.byteValue());
-					feed_dict.addUint8(1012, (byte) i);
-					sendData(context, feed_dict);
+			if (command_id != null) {
+
+				if (command_id.intValue() == 0) { // hello
+					Integer total = _feeds.size();
+					if (total > 16)
+						total = 16; // clamp
+					for (int i = 0; i < total; i++) {
+						PebbleDictionary feed_dict = new PebbleDictionary();
+						feed_dict.addString(1001, substring(_feeds.get(i).getName(), maxLength));
+						feed_dict.addUint8(1011, total.byteValue());
+						feed_dict.addUint8(1012, (byte) i);
+						queueData(context, feed_dict);
+					}
+					sendData(context);
 				}
+
+				if (command_id.intValue() == 1) // ack
+					sendData(context);
+
 			}
 
 			Long feed_id = data.getUnsignedInteger(1091);
@@ -58,10 +74,11 @@ public class MainActivity extends Activity {
 					item_dict.addString(1002, substring(items.get(i).getTitle(), maxLength));
 					item_dict.addUint8(1011, total.byteValue());
 					item_dict.addUint8(1012, (byte) i);
-					sendData(context, item_dict);
+					queueData(context, item_dict);
 				}
 
 				_lastFeed = feed_id;
+				sendData(context);
 			}
 
 			Long item_id = data.getUnsignedInteger(1092);
@@ -70,8 +87,9 @@ public class MainActivity extends Activity {
 				FeedItem item = feed.getItems().get(item_id.intValue());
 				String content = item.getContent();
 				int parts = (int) Math.ceil((double) content.length() / maxLength);
-				if (parts > 10)
-					parts = 1000 / maxLength; // clamp
+				int maxParts = 1000 / maxLength;
+				if (parts > maxParts)
+					parts = maxParts; // clamp
 				for (int offset = 0; offset < (parts * maxLength); offset += maxLength) {
 					int length = content.length() - offset;
 					if (length > maxLength)
@@ -80,25 +98,27 @@ public class MainActivity extends Activity {
 					message_dict.addString(1003, content.substring(offset, offset + length));
 					message_dict.addUint8(1011, (byte) parts);
 					message_dict.addUint16(1012, (short) offset);
-					sendData(context, message_dict);
+					queueData(context, message_dict);
 				}
+				sendData(context);
+			}
+
+		}
+
+		private void queueData(Context context, PebbleDictionary dictionary) {
+			Log.d("queueData", dictionary.toJsonString());
+			_msgQueue.add(dictionary);
+		}
+
+		private void sendData(Context context) {
+			if (!_msgQueue.isEmpty()) {
+				PebbleDictionary dictionary = _msgQueue.remove();
+				Log.d("sendData", dictionary.toJsonString());
+				PebbleKit.sendDataToPebbleWithTransactionId(context, APP_UUID, dictionary, _transactionId++ % 255);
 			}
 		}
 
-		private void sendData(Context context, PebbleDictionary dict) {
-			Log.d("sendData", dict.toJsonString());
-			// PebbleKit.sendDataToPebble(context, APP_UUID, dict);
-			PebbleKit.sendDataToPebbleWithTransactionId(context, APP_UUID, dict, ++_lastTransactionId % 255);
-		}
-
 	};
-
-	private String substring(String str, int maxLength) {
-		int length = str.length();
-		if (length > maxLength)
-			length = maxLength;
-		return str.substring(0, length);
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
